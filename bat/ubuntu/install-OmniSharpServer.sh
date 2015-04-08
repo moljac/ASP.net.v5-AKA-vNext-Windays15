f9bcf0729b5d4785d97639d49d53b9b67855d97c
moljac <mcvjetko@holisticware.net>
2015-04-08 23:54:05 +0200

    OmniSharp compile script corrections

--- bat/ubuntu/install-OmniSharpServer.sh
+++ bat/ubuntu/install-OmniSharpServer.sh
@@ -4,7 +4,10 @@ rmdir OmniSharpServer
 git clone https://github.com/nosami/OmniSharpServer.git
 cd OmniSharpServer
 git submodule update --init --recursive
-xbuild
 
+xbuild /p:Configuration=Debug
+# broken
+# xbuild /p:Configuration=Release
 
-mono OmniSharpServer.exe
+mono OmniSharp/bin/Debug/OmniSharp.exe -v
+
