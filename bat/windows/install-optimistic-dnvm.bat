abcf7426b84ca884c153cde86782e43e3345b32c
moljac <mcvjetko@holisticware.net>
2015-04-17 11:55:05 +0200

    windows install script update

--- bat/windows/install-optimistic-dnvm.bat
+++ bat/windows/install-optimistic-dnvm.bat
@@ -1,6 +1,9 @@
 
 @powershell ^
-	-NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"			
+	-NoProfile ^
+	-ExecutionPolicy unrestricted ^
+	-Command ^
+	"&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"			
 
 	pause
 			
