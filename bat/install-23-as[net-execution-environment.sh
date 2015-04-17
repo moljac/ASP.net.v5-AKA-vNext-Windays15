e8ea1d864e56b2c86d62d94c4115f56c6db4c6a7
moljac <mcvjetko@holisticware.net>
2015-04-17 15:53:10 +0200

    execution environment script

---
+++ bat/install-23-as[net-execution-environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERSION_MANAGERS=\
+    kvm \
+    dnvm 
+
+
+	
+
+for v in $VERSION_MANAGERS; do
+    VERSION_MANAGER=$v
+	echo item: $VERSION_MANAGER
+	
+    if [ -x "$(command -v $VERSION_MANAGER)" ]; then
+    	$VERSION_MANAGER upgrade
+    	$VERSION_MANAGER list
+    fi
+done
+
