a66d4c64d2fbe2bbe1cfcf742d554b2f258eb35d
moljac <mcvjetko@holisticware.net>
2015-04-17 11:54:27 +0200

    ubuntu  script renaming

--- bat/ubuntu/install-11-runtime-mono-from-source-github.sh
+++ bat/ubuntu/install-11-runtime-mono-from-source-github.sh
@@ -20,11 +20,41 @@ sudo apt-get -y install \
 	gettext \
 	build-essential \
 	mono-devel \
-		
+
+
+
 	
+#=======================================================================================
+# In order to use mcs and mono binaries during the build process which do not 
+# reside in your PATH, you can set two make variables, 
+# 		EXTERNAL_MCS 
+#	and 
+#		EXTERNAL_RUNTIME
+# 	:
+# make EXTERNAL_MCS=/foo/bar/mcs EXTERNAL_RUNTIME=/somewhere/else/mono
+#-----------------------------------------------------------
+
+$PREFIX=/usr/local
 git clone https://github.com/mono/mono.git
 cd mono
 ./autogen.sh --prefix=$PREFIX
+
+
+
+#-----------------------------------------------------------
+# 
+# *** The compiler 'mcs' doesn't appear to be usable.
+# *** You need Mono version 3.8 or better installed to build MCS
+# *** Check mono README for information on how to bootstrap a Mono installation.
+# *** The version of 'mcs' is: Mono C# compiler version 3.2.8.0.
+# If you do not currently have mono installed, build and install mono from a 
+# recently released tarball or run:
+# ./autogen.sh must be run prior to this
+make get-monolite-latest
+
+
+#-----------------------------------------------------------
+# build mono
 make
 
 # installation through packages
