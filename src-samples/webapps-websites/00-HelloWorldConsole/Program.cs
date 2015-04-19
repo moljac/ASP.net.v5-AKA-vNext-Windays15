63ac9233413d503571cd3b5f05e44a936dff0bad
moljac <mcvjetko@holisticware.net>
2015-04-19 16:38:18 +0200

    Console Hello World

---
+++ src-samples/webapps-websites/00-HelloWorldConsole/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq.Expressions;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.ForegroundColor = ConsoleColor.Yellow;
+		Console.WriteLine("Hello Windays 15 - from K Runtime !");
+		
+		return;
+	}
+}
