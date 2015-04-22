13628d54e9de12f5f6be246fca782e3432d0afb2
moljac <mcvjetko@holisticware.net>
2015-04-22 07:20:52 +0200

    servers scripts install firefly

---
+++ bat/linux/asp-net-v5-vNext/servers/install-firefly.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://github.com/FireflyServer/firefly
+# https://github.com/FireflyServer/firefly/archive/master.zip
+
+git clone https://github.com/FireflyServer/firefly.git
+
+https://github.com/FireflyServer/firefly/blob/master/build.sh
