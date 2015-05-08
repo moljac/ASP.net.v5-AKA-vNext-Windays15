3f4c145c3c96365d7573a6085f158346c1e10f87
moljac <mcvjetko@holisticware.net>
2015-05-08 20:25:29 +0200

    MVC sample for weblica

--- src-samples/webapps-websites/21-ASP.net-Home-DevTools/scaffolding-yeoman/EmptyApplication/Startup.cs
+++ src-samples/webapps-websites/21-ASP.net-Home-DevTools/scaffolding-yeoman/EmptyApplication/Startup.cs
@@ -1,20 +1,24 @@
-using System;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 
-namespace EmptyApplication
+namespace HelloMvc
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseStaticFiles();
+            app.UseErrorPage();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.UseWelcomePage();
         }
     }
