fd285f067af9dcc4ea7c0ffe1fd085bc87ef574e
moljac <mcvjetko@holisticware.net>
2015-06-17 21:57:52 +0200

    nginx server start commands

--- bat/linux/Debian/raspberry/install-41-server-http-nginx.sh
+++ bat/linux/Debian/raspberry/install-41-server-http-nginx.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # First install the nginx package
-sudo apt-get install \
-	nginx
+sudo apt-get -y \
+	install  \
+		nginx
 
 # start the server with:
 sudo \
 	/etc/init.d/nginx start
+	
+	
+# make nginx service/deamon to be default service (started at boot time)
+sudo \
+	update-rc.d nginx defaults
+
+# reboot to see if nginx starts
+sudo \
+	reboot
