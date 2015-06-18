f3d3fcac714abdbe43253796c9ade2744fdd332a
moljac <mcvjetko@holisticware.net>
2015-06-18 08:46:03 +0200

    nginx server installation script update

--- bat/linux/Debian/raspberry/install-41-server-http-nginx.sh
+++ bat/linux/Debian/raspberry/install-41-server-http-nginx.sh
@@ -5,11 +5,20 @@ sudo apt-get -y \
 	install  \
 		nginx
 
+		
+# Create cache directory for Nignx webserver:
+
+sudo \
+	mkdir /var/cache/nginx
+sudo \
+	chown www-data:www-data /var/cache/nginx	
+		
 # start the server with:
 sudo \
 	/etc/init.d/nginx start
 	
-	
+ifconfig eth0 | grep inet | awk '{ print $2}'
+ 
 # make nginx service/deamon to be default service (started at boot time)
 sudo \
 	update-rc.d nginx defaults
@@ -17,3 +26,6 @@ sudo \
 # reboot to see if nginx starts
 sudo \
 	reboot
+	
+	
+	
